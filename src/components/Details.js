516f957ba75d3542c4640f3b3e6b89e4fdd95f83
Ehab Hashem <ehab.hashem@almotahida.local>
2023-11-28 18:17:51 +0200

    render items while useState is true

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,17 +3,29 @@ import { useParams } from 'react-router-dom'
 
 function Details() {
     const {id} = useParams();
-    const [data, setData] = useState();
-  useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/${id}`)
-    .then(res=>res.json())
-    .then((data) => setData(data))
-  }, [])
   
-console.log(data)
+
+  const [test, setTest] = useState(["aly","ehab","taher"]);
+  const [refresh, setRefresh] = useState(false);
+  useEffect(() => {
+    setTest([...test,"mahmoud"])
+  }, [refresh])
+  
+const add = ()=>{
+setRefresh(!refresh)
+}
+
   return (
     <div>
       <p className='text-center'>{id}</p>
+      <div>
+        {test.map(item=>(
+          <div>
+            <p>{item}</p>
+          </div>
+        ))}
+      </div>
+      <button onClick={add}>click</button>
     </div>
   )
 }
